54e5af448d40545921b6990e9a4942b4c9241afa
jayaantara10 <jayaantara10@gmail.com>
2021-12-23 16:21:55 +0800

    nambahin toast aja di dashboard

--- app/src/main/java/id/jayaantara/ibaca/DashboardActivity.java
+++ app/src/main/java/id/jayaantara/ibaca/DashboardActivity.java
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
@@ -52,5 +53,41 @@ public class DashboardActivity extends AppCompatActivity {
     @Override
     public void onBackPressed() {
         ActivityCompat.finishAffinity(this);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "Hallo dude", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, "See you letter", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(this, "Hai welcomeback", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "In here you can search many thing that you can learn", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, "Okey i,am waiting for you", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "Goodbye dude", Toast.LENGTH_SHORT).show();
     }
 }
